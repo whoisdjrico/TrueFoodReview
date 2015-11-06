a93f5ba3b54143c18345bbea9871b743a74f3c6f
Susan Tashiro <susantashiro@gmail.com>
2015-11-06 12:57:06 -0800

    commit before pulling from upstream

---
+++ client/app.js
@@ -0,0 +1,21 @@
+var app = angular
+  .module('myApp',[
+    'ui.router',
+    'TrueFoodReview.InfoFactory',
+    'TrueFoodReview.HomeController'
+
+    ]);
+
+app.config(configFunction);
+
+function configFunction($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('state1', {
+      url: '/',
+      templateUrl: './partials/home.html',
+      controller: 'HomeController'
+    })
+  
+}
+
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var yelpController = require('./yelpController');
 
 
 
 app.get('/yelp', yelpController.getData);
-// app.get('/linkedIn', linkedInController.getData);
 
-
+app.use(express.static(path.join(__dirname, './../client/')));
 
 
 app.listen(3000);
