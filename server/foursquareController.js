15463d46bcc2310d82f1bcdb5ae2400a1316ed20
Leonard Kee <leonardwkee@gmail.com>
2015-11-06 10:36:20 -0800

    foursquare additions

---
+++ server/foursquareController.js
@@ -0,0 +1,10 @@
+var foursquare = (require('foursquarevenues'))('2QFBHZM23MLCX0WRQATSMNV1XSV41LONUF1AOUEHADZVZZBI', '5TXGRZBINMI4KWOPFFELJ1UUUP3YMRM0VKZJ4XKMRTMQZ4XU');
+ 
+function fourSquareVenues(req, res, next) {
+	
+
+}
+
+    // get request in the url returns the huge data on the site
+    // learn the parameters of the foursquare
+    // brush up on async
