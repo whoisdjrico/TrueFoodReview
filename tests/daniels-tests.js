43d22f167b9d6a24fbb136f79a9754782c3a62f2
danieldealday <danieldealday@gmail.com>
2015-11-09 21:07:31 -0800

    added test for dataController to check if data was served

--- tests/daniels-tests.js
+++ tests/daniels-tests.js
@@ -1,19 +1,7 @@
 var test = require('tape');
-var Promise = require('bluebird');
-var mydb = require('./postgres.js');
-var Sequelize = require('sequelize');
-var server = require('./server/server.js');
-var yelpController = require('./../yelpController');
-var dataController = require('./../dataController');
-var foursquareController = require('./../foursquareController');
+var dataController = require('../server/dataController');
 
-//determine if application data was send to client from server
-test('foursquare api data request', function(t) {
-  t.plan(1);
-
-  t.equal(foursquareController.getData, 'result');
-
-  t.pass('data was received from foursquare api');
-  t.fail('data was not received from the foursquare api, please review code')
+test('DATA OUTPUT SUCCESS FROM dataController', function (t) {
+  t.notDeepLooseEqual(dataController, {});
   t.end();
 });
