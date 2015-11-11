53ad0272bd59a7dfc4a853d26af8f1c77810ac3e
danieldealday <danieldealday@gmail.com>
2015-11-10 20:04:00 -0800

    added new controllers

---
+++ server/googlemapsController.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+var googlemapsController = {
+
+  getData: function (req, res, next) {
+    request('https://www.googleapis.com/fusiontables/v1/tables/1997wo1poMe6iv7VXz1P5Dj1fkO-vud48QgNZwu1J?key=', function (error, res, data) {
+      if (error) console.log(error);
+      console.log(data);
+    });
+  }
+
+};
+
+module.exports = googlemapsController;
