b5087240b92fd02761c317c2a28d59826b4e0e24
danieldealday <danieldealday@gmail.com>
2015-11-10 18:34:23 -0800

    added new open table api call, started googlemaps.js and added new test for data requests

--- tests/daniels-tests.js
+++ tests/daniels-tests.js
@@ -1,7 +1,16 @@
 var test = require('tape');
 var dataController = require('../server/dataController');
+var opentableController = require('../server/opentableController');
+var path = require('path');
 
+//verify data output from Postgres clears as a data object
 test('DATA OUTPUT SUCCESS FROM dataController', function (t) {
   t.notDeepLooseEqual(dataController, {});
+  t.end();
+});
+
+//for use on api requests for json before controller completion as middleware
+test('API data request successful', function (t) {
+  t.notDeepLooseEqual(opentableController.getData, "application/json");
   t.end();
 });
