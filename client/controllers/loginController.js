4cd6349084b9d5fac7e037577a26dca03906023d
jshuadvd <jshuadvd@icloud.com>
2015-11-12 14:22:38 -0800

    Added POST requests to login and signup controllers

--- client/controllers/loginController.js
+++ client/controllers/loginController.js
@@ -1,18 +1,39 @@
 // 'use strict';
 
 var app = angular
-  .module('TrueFoodReview.LoginController', [])
-  .controller('LoginController', function() {
+  .module('TrueFoodReview.LoginController', ['ui.router'])
+  .controller('LoginController', ['$scope', '$http', LoginController])
 
-      this.isUserLoggedIn = false;
-      this.login = function() {
-      console.log("Sending Request to server...");
-        this.isUserLoggedIn = true;
-      // if (this.loginForm.$valid) {
-      //   console.log("Sending Request to server...");
-      // }
-      // else {
-      //   this.loginForm.submitted = true;
-      // }
-  };
-});
+function LoginController($scope, $http) {
+	var vm = this;
+	vm.isUserLoggedIn = false;
+	vm.login = function () {
+		this.isUserLoggedIn = true;
+
+		console.log("Sending Request to server...");
+
+		var data = $.param({
+			firstName: vm.firstName,
+			lastName: vm.lastName,
+			email: vm.email,
+			password: vm.password
+		});
+
+		var config = {
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+			}
+		}
+
+		$http.post('/signup', data, config)
+			.success(function (data, status, headers, config) {
+				vm.PostDataResponse = data;
+			})
+			.error(function (data, status, header, config) {
+				vm.ResponseDetails = "Data: " + data +
+					"<hr />status: " + status +
+					"<hr />headers: " + header +
+					"<hr />config: " + config;
+		});
+	};
+};
