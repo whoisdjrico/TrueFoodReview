45558d5e6c73304b13e6add81270f33b309773f9
danieldealday <danieldealday@gmail.com>
2015-11-12 10:43:58 -0800

    commit preparation for changes

--- server/opentableController.js
+++ server/opentableController.js
@@ -1,29 +1,34 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 var request = require('request');
 
-var opentableController = {
+var openTableController = {
+  getData: function(req, res, next) {
+    request('https://opentable.herokuapp.com/api/restaurants?postal_code=90212', function(error, res, data) {
+      if(error) console.log(error);
 
-  getData: function (req, res, next) {
-    request('http://opentable.herokuapp.com/api/restaurants?zip=90094', function (error, res, data) {
-      if (error) console.log(error);
-      var apiData = JSON.parse(data);
-      var migrate = [];
-      apiData.restaurants.forEach(function(item) {
-        var obj = {};
-        obj.name = item.name,
-        obj.address = item.address,
-        obj.city = item.city,
-        obj.state = item.city,
-        obj.postal_code = item.postal_code,
-        obj.phone = item.phone,
-        obj.reserve_url = item.reserve_url;
-        obj.mobile_reserve_url = item.reserve_url,
-        obj.image_url = item.image_url
-        migrate.push(obj);
-      });
-      console.log(migrate);
+      var openTableData = JSON.parse(data);
+      console.log(openTableData);
+
+      var full = [];
+      // openTableData.restaurants.forEach(function(item){
+      //   var obj = {};
+      //   obj.name = item.name;
+      //   obj.address = item.address;
+      //   obj.city = item.city;
+      //   obj.state = item.state;
+      //   obj.postal_code = item.postal_code;
+      //   obj.phone = item.phone;
+      //   obj.reserve_url = item.reserve_url;
+      //   obj.mobile_reserve = item.mobile_reserve;
+      //   obj.image_url = item.image_url;
+      //   full.push(obj);
+      //   // console.log(JSON.stringify(obj));
+      // });
     });
   }
+}
 
-};
 
-module.exports = opentableController;
+module.exports = openTableController;
