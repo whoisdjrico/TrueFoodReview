136b413d8938043478463fb7e02a41f8cfaedd73
jshuadvd <jshuadvd@icloud.com>
2015-11-12 16:58:13 -0800

    Added Home page and fixed controller routes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var yelpController = require('./yelpController');
 var dataController = require('./dataController');
 var fusionTableController = require('./fusionTableController');
 var foursquareController = require('./foursquareController');
+// var signUpController = require('../client/controllers/signUpController');
 var config = require('../config.json');
 
 // Test to get data from the fusion table
@@ -14,7 +15,7 @@ app.get('/fusion', fusionTableController.getData);
 app.get('/data', yelpController.getData, foursquareController.getData, dataController);
 // app.get('foursquare', foursquareController.getData);
 app.use(express.static(path.join(__dirname, './../client/')));
-// app.post('/signup', SignUpController);
+// app.post('/signup', signUpController.signup);
 app.get('/', function(req, res) {
   res.render('index.html');
 });
